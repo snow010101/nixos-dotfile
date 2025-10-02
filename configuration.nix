55de4a9dfefa7490ca9f02b2bda1a4e6b137393f
snowflake <utkadobraykurtka@duck.com>
2025-10-02 19:37:34 +0300

    добавлен niri и всякие ништяки по типу шрифтов

--- configuration.nix
+++ configuration.nix
@@ -84,7 +84,6 @@
   # Enable the KDE Plasma Desktop Environment.
   services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
-
   # Configure keymap in X11
   services.xserver.xkb = {
     layout = "us";
--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
     # to have it up-to-date or simply don't specify the nixpkgs input
     inputs.nixpkgs.follows = "nixpkgs";
   };
-    # Укажите здесь желаемый канал nixpkgs.
+  # Укажите здесь желаемый канал nixpkgs.
     # Замените "nixos-23.11" на вашу версию из system.stateVersion в configuration.nix
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
@@ -19,10 +19,24 @@
       system = "x86_64-linux"; # Для ARM-систем укажите "aarch64-linux"
       modules = [
         ({ config, pkgs, ... }: {
+	fonts.packages = with pkgs; [
+ 	  nerd-fonts.fira-code
+	  nerd-fonts.iosevka
+ 	  nerd-fonts.droid-sans-mono
+	 ];
+	  fonts.fontconfig.enable = true;
           programs.steam.enable = true;
           programs.fish.enable = true;
+          programs.niri.enable = true;
+          programs.kdeconnect.enable = true;
           environment.systemPackages = with pkgs; [
             neovim
+	    swww
+	    obs-studio
+	    waybar
+	    rofi
+            kitty
+            font-awesome
             inputs.zen-browser.packages."${system}".default
             fastfetch
             htop
